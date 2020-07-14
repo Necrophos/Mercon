73ebc20385d26a229fcad9253eade95265da5ee1
tuankhoi911 <17520648@gm.uit.edu.vn>
2020-07-14 10:16:23 +0700

    change tag

--- src/app/routes/general/general.module.ts
+++ src/app/routes/general/general.module.ts
@@ -1,18 +1,17 @@
 import { TabShipmentModule } from "./../../components/tab-shipment/tab-shipment.module";
 import { TabContainerModule } from "./../../components/tab-container/tab-container.module";
 import { TabGeneralModule } from "./../../components/tab-general/tab-general.module";
-import { TabsModule } from "ngx-tabs";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { GeneralComponent } from "./general.component";
-import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [GeneralComponent],
   imports: [
     CommonModule,
-    TabsModule,
+    NgbModule,
     TabGeneralModule,
     TabContainerModule,
     TabShipmentModule,
